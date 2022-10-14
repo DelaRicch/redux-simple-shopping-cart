f89f4388f766689e0f5e123391f5e8afead575f6
Dela Ricch <DelaRicch@gmail.com>
2022-10-14 04:46:04 +0000

    Done with navbar component

--- src/component/nav/NavBar.jsx
+++ src/component/nav/NavBar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Logo from '../../assets/logo.png'
+import { BsCart4 } from 'react-icons/bs'
+
+import './NavBar.css'
 
 const NavBar = () => {
   return (
     <nav>
         <div className="logo">
             <img src={Logo} alt={Logo} />
+        </div>
+
+        <div className="cart">
+            <BsCart4 className='cart-icon' />
+            <span>10</span>
         </div>
     </nav>
   )
